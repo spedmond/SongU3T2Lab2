e2b01105fc2fc5ed6ec90fe760a12129b5517595
unknown <BT_3W2_25@BKTECH.LOCAL>
2022-10-28 12:28:33 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int num = myScanner.nextInt();
+
+        if (num <= 0) {
+            System.out.println("That's not positive!");
+        }
+        else {
+            if (num%2 == 1) {
+                if (num%5 == 0 && num%7 == 0) {
+                    System.out.println("Divisible by 5!");
+                    System.out.println("Divisible by 7!");
+                }
+                else if (num%5 == 0) {
+                    System.out.println("Divisible by 5!");
+                }
+                else if (num%7 == 0) {
+                    System.out.println("Divisible by 7!");
+                }
+                else {
+                    System.out.println("Not divisible by 5 or 7");
+                }
+            }
+            else {
+                if (num >= 1000) {
+                    System.out.println("Big even number!");
+                }
+                else if (num >= 100 && num < 1000) {
+                    System.out.println("Medium even number!");
+                }
+                else {
+                    System.out.println("Small even number!");
+                }
+            }
+        }
+
+        // write your code here to print the appropriate message(s)
+        // described below
+
+    }
+}
